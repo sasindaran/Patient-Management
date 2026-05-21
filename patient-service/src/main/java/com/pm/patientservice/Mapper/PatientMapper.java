22891d0aa4ccf5061c55d550a1575bd1bd7127a0
sasindaran s <sasisam007@gmail.com>
2026-05-21 08:55:16 +0530

    dto,mapper,model is done

---
+++ patient-service/src/main/java/com/pm/patientservice/Mapper/PatientMapper.java
@@ -0,0 +1,20 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+import java.time.LocalDate;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setName(patient.getName());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+
+        return patientDTO;
+    }
+
+
+}
